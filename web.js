70bab68912e95e7ed4db93b93ecdf614b2ad2cf4
Daniel Yanisse <daniel.yanisse@mogreet.com>
2013-03-11 19:30:08 -0700

    added mongo

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongo = require('mongodb');
 
 var app = express.createServer(express.logger());
 
@@ -9,4 +10,15 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/mydb'; 
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
 });
