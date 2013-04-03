13deaff23d401a30c16ea9672c547fc049c18238
Daniel Yanisse <daniel.yanisse@mogreet.com>
2013-04-03 01:29:59 -0700

    added users and auth twitter

--- app.js
+++ app.js
@@ -4,14 +4,24 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-	, deck = require('./routes/deck')
   , http = require('http')
+  , fs = require('fs')
   , path = require('path')
-	, mongo = require('mongodb');
+	, mongo = require('mongodb')
+	, mongoose = require('mongoose')
+	, config = require('./config')
+	, passport = require('passport');
+	
+var models_path = __dirname + '/models';
+fs.readdirSync(models_path).forEach(function (file) {
+	 require(models_path+'/'+file);
+});
 
 var app = express();
+
+var routes = require('./routes')
+, user = require('./routes/user')
+, deck = require('./routes/deck');
 
 /**
  * App configuration
@@ -25,8 +35,12 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.cookieParser());
+	app.use(express.session({ secret: config.express.session_secret }));
+  app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
 });
 
 app.configure('development', function(){
@@ -38,11 +52,16 @@ app.configure('development', function(){
  */
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 app.post('/decks', deck.create);
 app.get('/decks/:id', deck.show);
 app.put('/decks/:id', deck.update);
-
+app.get('/login', user.login);
+app.get('/signup', user.signup);
+app.get('/logout', user.logout);
+app.get('/users/:userId', user.show);
+app.get('/auth/twitter', passport.authenticate('twitter', { failureRedirect: '/login' }), user.signin);
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), user.authCallback);
+app.param('userId', user.user)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
@@ -52,13 +71,59 @@ http.createServer(app).listen(app.get('port'), function(){
 /**
  * Mongo config
  */
+
 var mongoUri = process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
-  'mongodb://localhost/mydb'; 
+  'mongodb://localhost/mydb';
+mongoose.connect(mongoUri);
 
-//mongo.Db.connect(mongoUri, function (err, db) {
-//  db.collection('presentations', function(er, collection) {
-//    collection.insert({'name': 'test'}, {safe: true}, function(er,rs) {
-//    });
-//  });
-//});
+
+
+/**
+ * Passport authentication
+ */
+
+var passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+
+var User = mongoose.model('User');
+
+passport.use(new TwitterStrategy({
+    consumerKey: config.twitter.consumer_key,
+    consumerSecret: config.twitter.consumer_secret,
+    callbackURL: "http://localhost:5000/auth/twitter/callback"
+  },
+
+  function(token, tokenSecret, profile, done) {
+    User.findOne({ 'twitter.id': profile.id }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        user = new User({
+            name: profile.displayName
+          , username: profile.username
+          , provider: 'twitter'
+          , twitter: profile._json
+        });
+        user.save(function (err) {
+          if (err) console.log(err);
+          return done(err, user);
+        });
+      }
+      else {
+				console.log("user signed in: "+JSON.stringify(user))
+        return done(err, user);
+      }
+    });
+  }
+));
+
+// serialize sessions
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findOne({ _id: id }, function (err, user) {
+    done(err, user);
+  });
+});
--- config.js
+++ config.js
@@ -1,13 +1,19 @@
 var config = {}
 
+config.express = {};
+config.twitter = {};
 config.slideshare = {};
 config.mogreet = {};
+
+config.express.session_secret = 'Y1CQ48827b19n6Fm8X13zU7a3no2Tz'
+
+config.twitter.consumer_key = 'HtMmKS6nV7c0u118QoYmw'
+config.twitter.consumer_secret = 'J8JGTGJegZ1G6dzN2syMWdz3JVEIHzANtK1mWYfTEg'
 
 config.slideshare.host = 'www.slideshare.net'
 config.slideshare.endpoints = {};
 config.slideshare.endpoints.upload = '/api/2/upload_slideshow'
 config.slideshare.endpoints.get = '/api/2/get_slideshow'
-
 config.slideshare.api_key = 'lQv72N4h'
 config.slideshare.shared_secret = 'LiGvQRXM'
 config.slideshare.username = 'danielyanisse'
